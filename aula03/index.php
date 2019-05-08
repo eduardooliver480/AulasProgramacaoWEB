d0168bce14b6f1bf09f8790b4cbc47bc054ee755
eduardooliver480 <47834318+eduardooliver480@users.noreply.github.com>
2019-05-08 19:58:51 -0300

    Aula reposição 08/05
    Aula reposição 08/05

---
+++ aula03/index.php
@@ -0,0 +1,46 @@
+<?php
+
+$preco = 2;
+if($preco >= 20) {
+    echo "Não posso comprar!";
+}else{
+    echo 'Posso comprar!';
+}
+
+$nota = 7 
+echo "<br>";
+if($nota >= 7 && $nota <= 10){
+    echo 'Aprovado';
+}else if($nota >= 4){
+    echo 'Recuperacao';
+}else{
+    echo 'Reprovado';
+}
+
+echo "<br>";
+$contador = 0;
+
+while($contador <= 10){
+        echo $contador++;
+        echo "<br>";
+}
+
+for($i = 10; $i <=50; $i++;){
+    echo "<br>";
+    echo $i;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
